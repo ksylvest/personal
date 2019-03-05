b89ea524087c17d1afb3f5b387cdcc8a2c26756c
Kevin Sylvestre <kevin@ksylvest.com>
2019-03-05 09:22:32 -0800

    Resave various webpacker configuration diffs

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,5 @@
 const { environment } = require('@rails/webpacker');
-const typescript = require('./loaders/typescript');
+const typescript =  require('./loaders/typescript');
 
-const loader = environment.loaders.get('file');
-loader.test = /\.(jpg|jpeg|png|gif|tiff|ico|svg|eot|otf|ttf|woff|webp)$/i;
-
-environment.loaders.append('typescript', typescript);
-
+environment.loaders.prepend('typescript', typescript);
 module.exports = environment;
