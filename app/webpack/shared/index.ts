880b59bf0487bfbe95153b1c602dc5d9178d6941
Kevin Sylvestre <kevin@ksylvest.com>
2020-04-11 14:29:10 -0700

    Install sentry

--- app/webpack/shared/index.ts
+++ app/webpack/shared/index.ts
@@ -1,8 +1,4 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-import * as Rails from "@rails/ujs";
-import * as Turbolinks from "turbolinks";
-
-Rails.start();
-Turbolinks.start();
+import "./initializers";
