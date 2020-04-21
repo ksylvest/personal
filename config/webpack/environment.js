6a176809093b88e1602749ede8eef58c8f028986
Kevin Sylvestre <kevin@ksylvest.com>
2020-04-20 19:38:16 -0700

    Fix typescript with latest version of web pack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,2 @@
 const { environment } = require('@rails/webpacker');
-const typescript =  require('./loaders/typescript');
-
-environment.loaders.prepend('typescript', typescript);
 module.exports = environment;
