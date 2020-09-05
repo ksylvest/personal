e996a0a97f60f6eb282b9ca6d43eb1651135b9ff
Kevin Sylvestre <kevin@ksylvest.com>
2020-09-04 21:31:17 -0700

    Swap eslint for tslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["prettier", "@typescript-eslint"],
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
--- app/webpack/application/controllers/recaptcha_controller.ts
+++ app/webpack/application/controllers/recaptcha_controller.ts
@@ -11,17 +11,17 @@ declare const grecaptcha: IRecaptcha;
 export class RecaptchaController extends Controller {
   public static targets = ["field"];
 
-  public initialize() {
+  public initialize(): void {
     const key = this.data.get("key");
     const action = this.data.get("action");
 
-    if (!key) { return; }
+    if (!key) {
+      return;
+    }
 
     loadjs(`https://www.google.com/recaptcha/api.js?render=${key}`, () => {
       grecaptcha.ready(() => {
-        grecaptcha
-        .execute(key, { action })
-        .then((token) => {
+        grecaptcha.execute(key, { action }).then((token) => {
           const input = this.targets.find("field") as HTMLInputElement;
           input.value = token;
         });
