100a61d9a5918925f9fcc80253138614c3fece80
Kevin Sylvestre <kevin@ksylvest.com>
2020-10-01 10:22:31 -0700

    Re-generate icons for font-awesome free

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -8,25 +8,25 @@ RSpec.describe Attachment, type: :model do
     context 'with audio' do
       let(:attachment) { create(:attachment, :audio) }
 
-      it { expect(attachment.icon).to eql('fal fa-file-audio') }
+      it { expect(attachment.icon).to eql('fas fa-file-audio') }
     end
 
     context 'with image' do
       let(:attachment) { create(:attachment, :image) }
 
-      it { expect(attachment.icon).to eql('fal fa-file-image') }
+      it { expect(attachment.icon).to eql('fas fa-file-image') }
     end
 
     context 'with video' do
       let(:attachment) { create(:attachment, :video) }
 
-      it { expect(attachment.icon).to eql('fal fa-file-video') }
+      it { expect(attachment.icon).to eql('fas fa-file-video') }
     end
 
     context 'with other' do
       let(:attachment) { create(:attachment, :other) }
 
-      it { expect(attachment.icon).to eql('fal fa-file') }
+      it { expect(attachment.icon).to eql('fas fa-file') }
     end
   end
 end
