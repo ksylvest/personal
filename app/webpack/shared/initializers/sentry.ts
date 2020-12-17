f08757c32b7a19180d126516bc22a1b6fa5943e1
Kevin Sylvestre <kevin@ksylvest.com>
2020-12-17 11:55:35 -0800

    Use sentry traces

--- app/webpack/shared/initializers/sentry.ts
+++ app/webpack/shared/initializers/sentry.ts
@@ -1,13 +1,17 @@
 declare const ENVIRONMENT: string;
 declare const RELEASE: string;
 declare const SENTRY_DSN: string | null;
+declare const SENTRY_TRACES_SAMPLE_RATE: number;
 
 import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
 
 if (SENTRY_DSN && ENVIRONMENT !== "test" && ENVIRONMENT !== "development") {
   Sentry.init({
     dsn: SENTRY_DSN,
     environment: ENVIRONMENT,
+    integrations: [new Integrations.BrowserTracing()],
     release: RELEASE,
+    tracesSampleRate: SENTRY_TRACES_SAMPLE_RATE,
   });
 }
