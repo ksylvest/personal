3f835e9efa84cf4c10a8fadf484b1cee1bfb5470
Kevin Sylvestre <kevin@ksylvest.com>
2021-02-10 11:19:26 -0800

    Bump webpack from v5 to v6

---
+++ config/webpack/base.js
@@ -0,0 +1,7 @@
+const { merge, webpackConfig } = require("@rails/webpacker");
+
+const ForkTSCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+module.exports = merge(webpackConfig, {
+  plugins: [new ForkTSCheckerWebpackPlugin()],
+});
