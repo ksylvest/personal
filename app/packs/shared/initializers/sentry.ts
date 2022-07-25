7a37b97542094dbce8c77bf3227916f01e4f22e3
Kevin Sylvestre <kevin@ksylvest.com>
2022-07-25 13:19:00 -0700

    Trace sentry in the browser

--- app/packs/shared/initializers/sentry.ts
+++ app/packs/shared/initializers/sentry.ts
@@ -1,13 +1,17 @@
 declare const ENVIRONMENT: string;
 declare const RELEASE: string;
 declare const SENTRY_DSN: string | null;
+declare const SENTRY_TRACES_SAMPLE_RATE: number | null;
 
 import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
 
-if (SENTRY_DSN && ENVIRONMENT !== "test" && ENVIRONMENT !== "development") {
+if (SENTRY_DSN && ENVIRONMENT === "production") {
   Sentry.init({
     dsn: SENTRY_DSN,
     environment: ENVIRONMENT,
     release: RELEASE,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: SENTRY_TRACES_SAMPLE_RATE ?? undefined,
   });
 }
