58c8a39515ebf2ab05f9401c2478e237dede7016
Kevin Sylvestre <kevin@ksylvest.com>
2023-09-11 12:16:58 -0700

    Use BrowserTracing via @sentry/browser

--- app/packs/shared/initializers/sentry.ts
+++ app/packs/shared/initializers/sentry.ts
@@ -4,7 +4,7 @@ declare const SENTRY_DSN: string | null;
 declare const SENTRY_TRACES_SAMPLE_RATE: number | null;
 
 import * as Sentry from "@sentry/browser";
-import { BrowserTracing } from "@sentry/tracing";
+import { BrowserTracing } from "@sentry/browser";
 
 if (SENTRY_DSN && ENVIRONMENT === "production") {
   Sentry.init({
