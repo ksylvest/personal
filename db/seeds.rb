89e0b50124571193fec0f57014bd285e728e3572
Kevin Sylvestre <kevin@ksylvest.com>
2023-09-12 12:26:25 -0700

    Move seeds to avoid inclusion in Docker

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ YAML.load_file(Rails.root.join('db/seeds/portfolio.yml')).each do |attributes|
   Portfolio.find_or_create_by!(name: attributes['name']) do |portfolio|
     portfolio.summary = attributes['summary']
     attributes['images'].each do |filename|
-      file = Rails.root.join("db/seeds/portfolio/#{attributes['slug']}/#{filename}").open
+      file = Rails.root.join("doc/portfolio/#{attributes['slug']}/#{filename}").open
       portfolio.images.attach(io: file, filename: filename)
     end
   end
