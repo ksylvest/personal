b5385161d68b6f5345d5addc4faf5faab9f32f22
Kevin Sylvestre <kevin@ksylvest.com>
2023-09-18 19:21:22 -0700

    Swap to using dedicated containerize job

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,5 +1,7 @@
 RSpec.configure do |config|
   config.before(type: :system) do
-    driven_by :selenium, using: :headless_chrome, screen_size: [1280, 800]
+    driven_by :selenium, using: :headless_chrome do |options|
+      options.add_argument('--disable-dev-shm-usage')
+    end
   end
 end
