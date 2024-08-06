7adcba99db1269566fdc796a5668480843e8508f
Kevin Sylvestre <kevin@ksylvest.com>
2024-08-06 02:02:30 -0700

    Delete migrations for canonical URL

--- db/migrate/20240620090456_add_canonical_url_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddCanonicalUrlToPosts < ActiveRecord::Migration[7.1]
-  def change
-    add_column :posts, :canonical_url, :string
-  end
-end
--- db/migrate/20240620090502_add_canonical_url_to_pages.rb
+++
@@ -1,5 +0,0 @@
-class AddCanonicalUrlToPages < ActiveRecord::Migration[7.1]
-  def change
-    add_column :pages, :canonical_url, :string
-  end
-end
